7738cf157ecce0cb8a838ed33358b3b4c14a8a6c
Lucas Augusto Kepler <lucaskepler991@gmail.com>
2025-04-25 11:31:07 -0300

    change routes settings

--- users/urls.py
+++ users/urls.py
@@ -4,10 +4,10 @@ from . import views
 urlpatterns = [
     path('register/', views.Register, name='Cadastro'),
     path('login/', views.Login, name="Login"),
-    path('usuarios/<int:user_id>/', views.GetUserById, name="GetUserById"),  
-    path('usuarios/', views.ReturnAllUsers, name="ReturnAllUsers"), 
-    path('usuarios/<int:user_id>/delete/', views.DeleteUserById, name="DeleteUserById"), 
+    path('<int:user_id>/', views.GetUserById, name="GetUserById"),  
+    path('', views.ReturnAllUsers, name="ReturnAllUsers"), 
+    path('delete/<int:user_id>/', views.DeleteUserById, name="DeleteUserById"), 
     path('reset-password/', views.ResetPassword, name="ResetPassword"), 
     path('set-new-password/', views.SetNewPassword, name="SetNewPassword"),  
-    path("usuarios/edit/<int:user_id>", views.EditUser, name="EditUser")
+    path("edit/<int:user_id>", views.EditUser, name="EditUser")
 ]
