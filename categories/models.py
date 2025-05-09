5980bfbb5461c7e9b60815725dec01cbc1345bee
Lucas Augusto Kepler <lucaskepler991@gmail.com>
2025-05-09 11:51:58 -0300

    change categories models

--- categories/models.py
+++ categories/models.py
@@ -8,6 +8,7 @@ class Categories(models.Model):
     secao = models.ForeignKey(Section, on_delete=models.CASCADE, related_name='categorias')
     nome = models.CharField(max_length=100, null=False)
     icone_id = models.CharField(max_length=50, blank=True, null=True)
+    cor = models.CharField(max_length=7, blank=True, null=True)  # Novo campo para cor hexadecimal
     created_at = models.DateTimeField(default=now)
     updated_at = models.DateTimeField(auto_now=True)
 
